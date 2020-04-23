d15c3fa7b21fd8d0a764548c680aa11e2b061e80
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-04-23 19:01:48 +0300

    Mistakes resolved

--- mongo_db_hw.py
+++ mongo_db_hw.py
@@ -6,23 +6,13 @@ import re
 def read_data(csv_file, db):
     with open(csv_file, encoding='utf8') as csvfile:
         reader = csv.DictReader(csvfile)
-        db.insert_many(reader)
+        for row in reader:
+            row["Цена"] = int(row["Цена"])
+            db.insert_one(row)
     print(f"Данные из {csv_file} записаны в коллекцию {db.name}")
 
 
-def string_to_int(db):
-    # Приведение строки в число для функции sort
-    string_treated = 0
-    if string_treated == 0:
-        for row in db.find():
-            old_price = {"Цена": row["Цена"]}
-            new_price = {"$set": {"Цена": int(row["Цена"])}}
-            db.update_one(old_price, new_price)
-        string_treated += 1
-
-
 def find_cheapest(db):
-    string_to_int(db=db)
     # Сортировка
     print("\nСортировка исполнителей от меньшей к большей цене: ")
     for row in db.find().sort("Цена", 1):
@@ -30,16 +20,11 @@ def find_cheapest(db):
 
 
 def find_by_name(name, db):
-    string_to_int(db=db)
     # Поиск подстроки
     print(f"\nСортировка исполнителей c подстрокой '{name}': ")
-    for row in db.find().sort("Цена", 1):
-        pattern = re.compile(f"\w*\s*\w*({name.lower()})+\w*\s*\w*")
-        result = pattern.findall(row['Исполнитель'].lower())
-        if result:
-            print(f"Исполнитель: {row['Исполнитель']}, (цена: {row['Цена']})")
-        # else:
-        #     print("Результаты поиска не дали результатов")
+    regex = re.compile(fr"\w*\s*\w*({name})+\w*\s*\w*", re.IGNORECASE)
+    for row in db.find({"Исполнитель": {"$regex": regex}}).sort("Цена", 1):
+        print(f"Исполнитель: {row['Исполнитель']}, (цена: {row['Цена']})")
 
 
 if __name__ == '__main__':
